9a302a2baf59f3459d743cee27351a35743978a4
Bishal Deb <bishaldeb656@gmail.com>
2019-04-08 01:49:54 +0530

    Checkout Cart page added.

--- src/checkoutpages/CheckoutCart.js
+++ src/checkoutpages/CheckoutCart.js
@@ -2,16 +2,14 @@ import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import { Row, Col, Button, Steps, Card, Icon } from "antd";
-
+import naruto2 from "../resources/naruto2.jpg";
 const Step = Steps.Step;
 
 export default class Cart extends React.Component {
   state = {
     products: {
-      image: "https://www.adorama.com/alc/wp-content/uploads/2017/06/gopro.jpg",
-      title: "Nikon",
-      description:
-        "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+      name: "Just a random name to fill the space",
+      image: naruto2,
       days: 4,
       date: {
         start: "04 Jan'19",
@@ -26,10 +24,7 @@ export default class Cart extends React.Component {
     return (
       <Card
         style={{
-          margin: "20px 0",
-          width: "100%%",
-          borderRadius: "9px",
-          boxShadow: "0 0 10px 4px rgba(49, 196, 167, 0.09)"
+          margin: "20px 0"
         }}
       >
         <h2 style={{ textShadow: "0.5px 0 0", fontSize: "19px" }}>
@@ -72,6 +67,7 @@ export default class Cart extends React.Component {
             &#8377; {this.state.products.refund}
           </div>
         </p>
+        <br />
         <Button
           style={{
             background: "#23b195",
@@ -93,10 +89,10 @@ export default class Cart extends React.Component {
   }
   render() {
     return (
-      <div style={{padding:"20px "}}>
+      <div style={{ padding: "20px 8%" }}>
         <Row>
           <Col md={{ span: 14, offset: 5 }} sm={{ span: 22, offset: 1 }}>
-            <div className="checkout">Checkout</div>
+            <div style={{textAlign:"center", textShadow:"1px 0 0", fontSize:"30px", marginBottom:"19px"}}>Checkout</div>
             <Steps
               style={{ width: "100%", padding: "1% 0 50px 0" }}
               current={0}
@@ -138,16 +134,19 @@ export default class Cart extends React.Component {
               Add more products
             </Link>
           </Col>
-        </Row>
-        <Row gutter={16}>
           <Col
             style={{ margin: "20px 0" }}
-            md={16}
+            md={13}
             sm={{ span: 22, offset: 1 }}
           >
             <CartItem products={this.state.products} />
           </Col>
-          <Col md={8} sm={{ span: 18, offset: 3 }} xs={{ span: 22, offset: 1 }}>
+          <Col
+            lg={7}
+            md={8}
+            sm={{ span: 18, offset: 3 }}
+            xs={{ span: 22, offset: 1 }}
+          >
             {this.cartTotal()}
           </Col>
         </Row>
@@ -159,17 +158,39 @@ export default class Cart extends React.Component {
 class CartItem extends React.Component {
   render() {
     return (
-      <div className="cartItem">
-        <Card className="cartCard">
-          <img className="cartImg" src={this.props.products.image} />
-          <Row className="cartDes">
-            <div className="cartTitle">{this.props.products.title}</div>
-            <Col style={{ paddingRight: "3%" }} sm={12} md={12} xl={12}>
+      <Row style={{borderRadius: "9px",
+        boxShadow: "0 0 10px 4px rgba(49, 196, 167, 0.09)",}}>
+        <Col span={8}>
+          <img
+            style={{ width: "100%", height: 160 }}
+            src={this.props.products.image}
+          />
+        </Col>
+        <Col span={16} style={{ padding: "12px 15px" }}>
+          <Row
+            style={{
+              fontSize: "12px"
+            }}
+          >
+            <Col span={16}>
+              <h3
+                style={{
+                  fontSize: "14px",
+                  color: "#4a4a4a",
+                  marginBottom: "2%",
+                  overflow: "hidden"
+                }}
+              >
+                {this.props.products.name}
+              </h3>
+            </Col>
+            <Col lg={10} sm={12} xs={13} style={{ fontSize: "11.5px" }}>
               Refundundable deposit
               <br />
               <strong style={{ color: "#23b195" }}>
                 &#8377; {this.props.products.refund}
               </strong>
+              <br />
               <br />
               Rental period <br />
               <strong>
@@ -177,26 +198,33 @@ class CartItem extends React.Component {
                 {this.props.products.date.end}
               </strong>
             </Col>
-            <Col sm={12} md={12} xl={12}>
+            <Col lg={10} sm={12} xs={11} style={{ fontSize: "11.5px" }}>
               Rent <br />
               <strong>&#8377; {this.props.products.rent}</strong>
+              <br />
               <br />
               Days: <br />
               <strong>{this.props.products.days}</strong>
             </Col>
           </Row>
-          <div
-            style={{ float: "right", position: "absolute", top: 0, right: 0 }}
-          >
-            <Button style={{ border: 0 }}>
-              <Icon type="edit" />
-            </Button>
-            <Button style={{ border: 0 }}>
-              <Icon type="close" />
-            </Button>
-          </div>
-        </Card>
-      </div>
+        </Col>
+        <div
+          style={{
+            float: "right",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            overflow: "hidden"
+          }}
+        >
+          <Button style={{ border: 0 }}>
+            <Icon type="edit" />
+          </Button>
+          <Button style={{ border: 0 }}>
+            <Icon type="close" />
+          </Button>
+        </div>
+      </Row>
     );
   }
 }
